7c702e2f2c53614351f9591ce62ce16d2a4e28cf
Girmay <ink423103@yahoo.com>
2019-10-29 23:50:32 +0100

    component modified

---
+++ client/src/components/pages/blog/PostCommentListItem.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class PostCommentListItem extends Component {
+    
+  
+    render() {    
+      return (  
+          <React.Fragment>                  
+            <p />           
+          </React.Fragment>    
+        )
+    }
+  }
+  
+  
+  
+  
+  export default PostCommentListItem;
