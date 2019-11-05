88549360b2872bfb0fda9b6773d511812d8e9d95
Girmay <ink423103@yahoo.com>
2019-11-05 23:52:08 +0100

    backend entery point modified

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,6 +4,7 @@ const triangleRoute = require('./api/triangle/triangleRouter');
 const usersRoute = require('./api/blog/usersRouter');
 const postsRoute = require('./api/blog/postsRouter');
 const commentsRoute = require('./api/blog/commentsRouter');
+const authorsRoute = require('./api/blog/authorsRouter');
 const app = express();
 app.use(bodyParser());
 
@@ -12,6 +13,7 @@ app.use('/triangle', triangleRoute);
 app.use('/blogs/users', usersRoute);
 app.use('/blogs/posts', postsRoute);
 app.use('/blogs/comment', commentsRoute);
+app.use('/blogs/authors', authorsRoute);
 const port= 5000;
 app.listen(port, () =>{
   console.log(`Server listening on port ${port}`);
