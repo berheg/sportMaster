99e449e03c107dede5bbeafec74489eea73ce961
Girmay <ink423103@yahoo.com>
2019-11-18 23:19:43 +0100

    Comments item added

--- client/src/components/pages/blog/PostCommentListItem.jsx
+++ client/src/components/pages/blog/PostCommentListItem.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import List from './ListItem';
-import * as API from '../../../api/index';
+//import * as API from '../../../api/index';
 export class PostCommentListItem extends Component {
     
   state ={
-    comments: [{id:1, post_id:1, description: 'It is important'},{id:2, post_id:1, description: 'It is important'}],
+    comments: [{id:1, post_id:1, description: 'It is important'},{id:2, post_id:1, description: 'It is intersting'}],
     noError: true,
     errorStatus: '',
     filteredComments: []
   }
-  componentDidMount = async() => {
+  /*componentDidMount = async() => {
     const fetchedComments = await API.getFetchedComments();
     if(!fetchedComments.status){
       this.setState({comments: fetchedComments});
@@ -18,17 +18,17 @@ export class PostCommentListItem extends Component {
       this.setState({noError: true}, {errorStatus: fetchedComments.status});
     }
     
-  }
+  }*/
     render() {   
       const {post} = this.props;
-      this.setState({filteredComments :this.state.comments.filter(comment => comment.post_id === post.id)});
+      //this.setState({filteredComments :this.state.comments.filter(comment => comment.post_id === post.id)});
       return ( 
         <div>        
           <React.Fragment>                  
             <label> Post with Id: {post.id}</label> 
             {this.state.noError && <List  key = {post.id}title = {post.description} />}
             <label>Comments</label> 
-                { this.state.filteredComments.map(comment => {
+                { this.state.comments.map(comment => {
                   return( 
                     <React.Fragment>
                       <List key= {comment.id} title = {comment.description}/>     
