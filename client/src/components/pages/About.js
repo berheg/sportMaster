31584d227eabc4f23225ed86a05b83c80ff0de77
Girmay <ink423103@yahoo.com>
2019-11-22 23:22:50 +0100

    about component added

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This app consists front end and backend. The frontend is used to check the triangle and to post and fetch data from the database </p>
+      <p>You can add data to the blog posts table to authors, posts, postcomment and users.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
